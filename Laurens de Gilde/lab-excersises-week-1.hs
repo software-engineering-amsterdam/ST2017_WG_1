7cdd5a7933d4656f1b51900c4309b0215787d81d
Laurens de Gilde <laurens-g@live.nl>
2017-09-05 13:41:17 +0200

    Amend last commit

--- Laurens de Gilde/lab-excersises-week-1.hs
+++ Laurens de Gilde/lab-excersises-week-1.hs
@@ -132,7 +132,7 @@ checkReversalPrimes (xs) = checkPrimeReversal (primesList xs)
 
 takeSmallestPrime :: Int -> Int -> [Integer] -> Integer
 takeSmallestPrime x y ps | prime (sumPrime) = sumPrime
-					     | otherwise = takeSmallestPrime (x+1) (y+1) (tail ps)
+					     | otherwise = takeSmallestPrime (x+1) (y+1) (ps)
 						 where
 						  	sumPrime = sum ( drop x . take y $ ps)	
 -- Question: 'Do you have to test that your answer is correct? How could this be checked?'
