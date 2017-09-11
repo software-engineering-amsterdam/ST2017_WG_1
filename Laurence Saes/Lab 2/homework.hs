57417f41d4392344c436dd69fb8f71fbf9f5262d
Laurence Saes <l.saes@live.nl>
2017-09-11 19:33:17 +0200

    Working on 2

--- Laurence Saes/Lab 2/homework.hs
+++ Laurence Saes/Lab 2/homework.hs
@@ -1,36 +1,38 @@
 module Lecture2
 
-where 
+where
 
 import System.Random
+import Test.QuickCheck
+import Data.List
 import Test.QuickCheck
 
 getRandomInt :: Int -> IO Int
 getRandomInt n = getStdRandom (randomR (0,n))
 
 randomFlip :: Int -> IO Int
-randomFlip x = do 
+randomFlip x = do
    b <- getRandomInt 1
    if b==0 then return x else return (-x)
 
 genIntList :: IO [Int]
-genIntList = do 
+genIntList = do
   k <- getRandomInt 20
   n <- getRandomInt 10
   getIntL k n
 
 getIntL :: Int -> Int -> IO [Int]
 getIntL _ 0 = return []
-getIntL k n = do 
+getIntL k n = do
    x <-  getRandomInt k
    y <- randomFlip x
    xs <- getIntL k (n-1)
    return (y:xs)
 
-quicksort :: Ord a => [a] -> [a]  
-quicksort [] = []  
-quicksort (x:xs) = 
-   quicksort [ a | a <- xs, a <= x ]  
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+   quicksort [ a | a <- xs, a <= x ]
    ++ [x]
    ++ quicksort [ a | a <- xs, a > x ]
 
@@ -54,10 +56,10 @@ testR k n f r = if k == n then print (show n ++ " tests passed")
 testPost :: ([Int] -> [Int]) -> ([Int] -> Bool) -> IO ()
 testPost f p = testR 1 100 f (\_ -> p)
 
-quicksrt :: Ord a => [a] -> [a]  
-quicksrt [] = []  
-quicksrt (x:xs) = 
-   quicksrt [ a | a <- xs, a < x ]  
+quicksrt :: Ord a => [a] -> [a]
+quicksrt [] = []
+quicksrt (x:xs) =
+   quicksrt [ a | a <- xs, a < x ]
    ++ [x]
    ++ quicksrt [ a | a <- xs, a > x ]
 
@@ -67,7 +69,7 @@ samelength xs ys = length xs == length ys
 testRl :: ([Int] -> [Int]) -> ([Int] -> [Int] -> Bool) -> IO ()
 testRl f r = testR 1 100 f r
 
-infix 1 --> 
+infix 1 -->
 
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
@@ -85,24 +87,24 @@ hoareTestR ::  Fractional t =>
 hoareTestR precond f postcond testcases = let
        a = fromIntegral (length $ filter precond testcases)
        b = fromIntegral (length testcases)
-     in 
+     in
        (all (\x ->
          precond x --> postcond (f x)) testcases,a/b)
 
 invarTest :: (a -> Bool) -> (a -> a) -> [a] -> Bool
-invarTest invar f = hoareTest invar f invar             
+invarTest invar f = hoareTest invar f invar
 
 invarTestR ::  Fractional t =>
                (a -> Bool) -> (a -> a) -> [a] -> (Bool,t)
-invarTestR invar f = hoareTestR invar f invar             
+invarTestR invar f = hoareTestR invar f invar
 
-f1,f2 :: Int -> Int 
+f1,f2 :: Int -> Int
 f1 = \n -> sum [0..n]
 f2 = \n -> (n*(n+1)) `div` 2
 
 test = verboseCheck (\n -> n >= 0 ==> f1 n == f2 n)
 
-parity n = mod n 2    
+parity n = mod n 2
 
 testRel :: (a -> a -> Bool) -> (a -> a) -> [a] -> Bool
 testRel spec f = all (\x -> spec x (f x))
@@ -110,27 +112,27 @@ testRel spec f = all (\x -> spec x (f x))
 testInvar :: Eq b => (a -> b) -> (a -> a) -> [a] -> Bool
 testInvar specf = testRel (\ x y -> specf x == specf y)
 
-stronger, weaker :: [a] -> 
+stronger, weaker :: [a] ->
        (a -> Bool) -> (a -> Bool) -> Bool
 stronger xs p q = forall xs (\ x -> p x --> q x)
-weaker   xs p q = stronger xs q p 
+weaker   xs p q = stronger xs q p
 
 neg :: (a -> Bool) -> a -> Bool
 neg p = \ x -> not (p x)
 
-infixl 2 .&&. 
+infixl 2 .&&.
 infixl 2 .||.
 
 (.&&.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
-p .&&. q = \ x -> p x && q x 
+p .&&. q = \ x -> p x && q x
 
 (.||.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
 p .||. q = \ x -> p x || q x
 
 compar :: [a] -> (a -> Bool) -> (a -> Bool) -> String
-compar xs p q = let pq = stronger xs p q 
-                    qp = stronger xs q p 
-                in 
+compar xs p q = let pq = stronger xs p q
+                    qp = stronger xs q p
+                in
                   if pq && qp then "equivalent"
                   else if pq  then "stronger"
                   else if qp  then "weaker"
@@ -148,70 +150,62 @@ probs :: Int -> IO [Float]
 probs 0 = return []
 probs n = do
              p <- getStdRandom random
-             ps <- probs (n-1) 
+             ps <- probs (n-1)
              return (p:ps)
 
-{-
-increateGroup :: Float -> (Int,Int,Int,Int) -> (Int,Int,Int,Int)
-increateGroup x (q,w,e,r) = (q + q2, w + w2, e + e2, r + r2)
-				where  q2 = if (x >= 0 && x <= 0.25) then 1 else 0
-				       w2 = if (x > 0.25 && x <= 0.5) then 1 else 0
-				       e2 = if (x > 0.5 && x <= 0.75) then 1 else 0
-				       r2 = if (x > 0.75 && x <= 1) then 1 else 0
--}
 
 -- Exercise 1
 
 generateProps :: [Float] -> (Int,Int,Int,Int)
 generateProps [] = (0,0,0,0)
 generateProps (x:xs) = let (q,w,e,r) = generateProps xs in
-				(q + q2, w + w2, e + e2, r + r2)
-					where  q2 = if (x >= 0 && x <= 0.25) then 1 else 0
-					       w2 = if (x > 0.25 && x <= 0.5) then 1 else 0
-					       e2 = if (x > 0.5 && x <= 0.75) then 1 else 0
-					       r2 = if (x > 0.75 && x <= 1) then 1 else 0
+        (q + q2, w + w2, e + e2, r + r2)
+          where  q2 = if (x >= 0 && x <= 0.25) then 1 else 0
+                 w2 = if (x > 0.25 && x <= 0.5) then 1 else 0
+                 e2 = if (x > 0.5 && x <= 0.75) then 1 else 0
+                 r2 = if (x > 0.75 && x <= 1) then 1 else 0
 
 generatePropsI :: IO [Float] -> IO (Int,Int,Int,Int)
 generatePropsI fai = do fa <- fai
-			return (generateProps fa)
-			  
+                        return (generateProps fa)
+
 -- generatePropsI (probs 10000)
 -- (2514,2584,2535,2367)
 -- The distribution is very close. 25,1% - 25.84% - 25.35% - 23.67%
--- In the last slot it will 123 times 
+-- In the last slot it will 123 times
 
--- statistically significant 
+-- statistically significant
 -- It should have an exqual density.
 -- To disprove that the deferenince in the fractrions or significant enought that it is not
 -- by accedent that they are to high.
 
--- standart deviation 
+-- standart deviation
 -- 0.5
 -- truly uniform
 
 -- Exercise 2
 
-data Shape = NoTriangle | Equilateral 
+data Shape = NoTriangle | Equilateral
             | Isosceles  | Rectangular | Other deriving (Eq,Show)
 
 isTriangle :: Integer -> Integer -> Integer -> Bool
-isTriangle a b c = a + b > c && a + c > b && b + c > a
+isTriangle a b c = a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0
 
 isEquilateralTriangle :: Integer -> Integer -> Integer -> Bool
 isEquilateralTriangle a b c = (isTriangle a b c) && a == b && b == c
 
 isRectangular :: Integer -> Integer -> Integer -> Bool
-isRectangular a b c = (isTriangle a b c) && a * a + b * b == c * c
+isRectangular a b c = (isTriangle a b c) && a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a
 
 isIsosceles :: Integer -> Integer -> Integer -> Bool
 isIsosceles a b c = (isTriangle a b c) && not (isEquilateralTriangle a b c) && a == b || a == c || b == c
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle a b c = if not isTri then NoTriangle else
-                   if isEqu then Equilateral else 
-                     if isRec then Rectangular else 
-                       if isIso then Isosceles else 
-                         Other
+triangle a b c | not isTri = NoTriangle
+               | isEqu = Equilateral
+               | isRec = Rectangular
+               | isIso = Isosceles
+               | otherwise = Other
                    where isTri = isTriangle a b c
                          isEqu = isEquilateralTriangle a b c
                          isRec = isRectangular a b c
@@ -235,5 +229,54 @@ triangle a b c = if not isTri then NoTriangle else
 
 -- Exercise 3
 
+isPermutation :: (Ord a) => [a] -> [a] -> Bool
+isPermutation xs ys = sort xs == sort ys
+
+isNoSameIndexValues :: (Eq a) => [a] -> [a] -> Bool
+isNoSameIndexValues xs ys = all id (zipWith (\x y -> x /= y) xs ys)
+
+isSameLength :: [a] -> [a] -> Bool
+isSameLength xs ys = length xs == length ys
+
+isDerangement :: (Ord a) => [a] -> [a] -> Bool
+isDerangement xs ys = sameLenth && notSameIndexes && permutation
+                      where sameLenth = isSameLength xs ys
+                            notSameIndexes = isNoSameIndexValues xs ys
+                            permutation = isPermutation xs ys
+
+deran :: (Eq a) => [a] -> [[a]]
+deran xs = filter (/=xs) (permutations xs)
+
+propertyPermutation :: (Ord a) => [a] -> [a] -> Bool
+propertyPermutation xs ys = isDerangement xs ys --> isPermutation xs ys
+
+propertyNoSameIndexValues :: (Ord a) => [a] -> [a] -> Bool
+propertyNoSameIndexValues xs ys = isDerangement xs ys --> isNoSameIndexValues xs ys
+
+propertyisSameLength :: (Ord a) => [a] -> [a] -> Bool
+propertyisSameLength xs ys = isDerangement xs ys --> isDerangement xs ys
+
+totalDerangementsForSize :: Int -> Int
+totalDerangementsForSize x = 1 -- TODO HELP
+
+testRandomDerangement :: (Ord a) => [a] -> Bool
+testRandomDerangement xs = derangements == totalDerangementsForSize (length xs)
+                           where derangeList = map (\ys -> isDerangement xs ys) (permutations xs)
+                                 derangements = length (filter (==True) derangeList)
+
+-- quickCheck propertyPermutation
+-- quickCheck propertyNoSameIndexValues
+-- quickCheck propertyisSameLength
+
+tpro :: Int -> Bool -> Int
+tpro x f = length( filter (==f) (map (\ys -> isDerangement [1..x] ys) (permutations [1..x])))
 
 
+
+
+
+-- Zip the lists. drop while not equal and check if empty list
+-- Then foldr over list one
+
+-- No element in the lists can be the same on the same index
+-- It has to be a permutation of eachother
