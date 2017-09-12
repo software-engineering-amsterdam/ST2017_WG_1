8935eec077b48a3e5ccde9943d213666818aae83
eigenaar <lauwdude@hotmail.com>
2017-09-12 13:26:46 +0200

    CHanges

--- Exercises/lab-2.hs
+++ Exercises/lab-2.hs
@@ -1,5 +1,5 @@
 {-
-  Abel Kaandorp (pending)
+  Abel Kaandorp (12012149)
   Laurens de Gilde (11721863)
   Laurence Saes (11422629)
   Tim Vonsee (11233001)
@@ -414,7 +414,7 @@ testRandomDerangement xs = toInteger (derangements) == totalDerangementsForSize 
 -}
 
 rot13 [] = []
-rot13 (x:xs) | isLower x && (ord x + 13) > 122  = chr ((ord x + 13) - 26) : rot13 xs
+rot13 (x:xs) | isLower x && (ord x + 13) > 123  = chr ((ord x + 13) - 26) : rot13 xs
              | isUpper x && (ord x + 13) > 90 = chr ((ord x + 13) - 26) : rot13 xs
              | isLower x && (ord x + 13) > 96 && (ord x + 13) < 123 = chr (ord x + 13)  : rot13 xs
              | isUpper x && (ord x + 13) > 65 && (ord x + 13) < 90  = chr (ord x + 13) : rot13 xs
@@ -426,12 +426,23 @@ checkRot13 xs = xs == rot13(rot13 xs)
 -- Exercise 7
 -- Time 3 hours
 
+rearrange :: [Char] -> [Char]
+rearrange iban = drop 4 iban ++ take 4 iban
 
+cleanSpaces :: [Char] -> [Char]
+cleanSpaces [] = []
+cleanSpaces (letter:iban) | isSpace letter = cleanSpaces iban
+                          | otherwise = letter : cleanSpaces (iban)
+toAscii :: Char -> [Char]
+toAscii letter | isNumber letter = [letter]
+               | otherwise = show ((ord letter) - 55)
+convert :: [Char] -> [Char]
+convert iban =  concat $ map (toAscii) . cleanSpaces . rearrange $ iban
 
+check :: [Char] -> Bool
+check iban = (read(convert iban) :: Integer) `mod` 97 == 1
 
-
-
-
+-- quickCheck
 
 
 
