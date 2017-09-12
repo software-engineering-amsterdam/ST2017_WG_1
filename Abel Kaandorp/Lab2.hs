bfc23a4f98a2b86d331841e949a3a67ba9c4bb19
ajm <ajm@localhost.localdomain>
2017-09-12 12:51:44 +0200

    changes

--- Abel Kaandorp/Lab2.hs
+++ Abel Kaandorp/Lab2.hs
@@ -30,50 +30,79 @@ probs n = do
 
 
 
--- Exercise 2
+{- Exercise 2: Recognizing triangles
+
+Write a program (in Haskell) that takes a triple of integer values as arguments and gives as output one of the following statements:
+
+        Not a triangle (Geen driehoek) if the three numbers cannot occur as the lengths of the sides of triangle,
+
+        Equilateral (Gelijkzijdig) if the three numbers are the lengths of the sides of an equilateral triangle,
+
+        Rectangular (Rechthoekig) if the three numbers are the lengths of the sides of a rectangular triangle,
+
+        Isosceles (Gelijkbenig) if the three numbers are the lengths of the sides of an isosceles (but not equilateral) triangle,
+
+        Other (Anders) if the three numbers are the lengths of the sides of a triangle that is not equilateral, not rectangular, and not isosceles.-}
 
 data Shape = NoTriangle | Equilateral
            | Isosceles  | Rectangular | Other deriving (Eq,Show)
 
-pickTriangle :: Integer -> Integer -> Integer -> Shape
-pickTriangle x y z      | x == y, y == z, x == z = Equilateral
-                        | (x == y || y == z || x == z) && (x+y > z) && (y+z > x) && (x+z>y) = Isosceles
-                        | x^2 + y^2 == z^2 = Rectangular
-                        | (x + y > z) && (x + z > y) && (y + z > x) = Other
-                        | otherwise = NoTriangle
+isTriangle :: Integer -> Integer -> Integer -> Bool
+isTriangle a b c = a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0
 
---RandomTestTriangles :: Bool
---RandomTestTriangles test = (randomR)
+isEquilateralTriangle :: Integer -> Integer -> Integer -> Bool
+isEquilateralTriangle a b c = (isTriangle a b c) && a == b && b == c
 
--- Testing
--- To test the recognition of triangles, we generate arrays consisting of 3 numbers until all types of triangles have
--- been picked at least once.
+isRectangular :: Integer -> Integer -> Integer -> Bool
+isRectangular a b c = (isTriangle a b c) && a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a
 
+isIsosceles :: Integer -> Integer -> Integer -> Bool
+isIsosceles a b c = (isTriangle a b c) && not (isEquilateralTriangle a b c) && a == b || a == c || b == c
 
-{- let testTriangles 
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c | not isTri = NoTriangle
+               | isEqu = Equilateral
+               | isRec = Rectangular
+               | isIso = Isosceles
+               | otherwise = Other
+                   where isTri = isTriangle a b c
+                         isEqu = isEquilateralTriangle a b c
+                         isRec = isRectangular a b c
+                         isIso = isIsosceles a b c
 
-genRandomTestCases :: Int -> IO([Int])
-randomList 0 = return []
-randomList n = do
-  r  <- randomRIO (1,100)
-  rs <- randomList (n-1)
-  return (r:rs)
--}
+{- Exercise 2: Test
+To test the correctness of the program, we have devised a number of cases of which the program would need to succesfully
+assign to the right type of triangles. For example: A triangle with sides such as (2,2,2) needs to be assigned to
+the type 'Equilateral' and a triangle with sides such as (3,4,4) needs to be assigned to the type 'Isosceles'. There are also
+a number of exceptional cases which do need to be handled. The program could for example have an input which defines one or more
+sides of a triangle as a negative number. In this case the program needs to correctly state that the input does not result in a 
+triangle. -}
 
 triangleTest:: Bool
-triangleTest = all (\(a,b,c,shape) -> pickTriangle a b c == shape) testCases
-
+triangleTest = all (\(a,b,c,shape) -> triangle a b c == shape) testCases
 
 testCases = [(2,2,2, Equilateral), 
-        (3,4,4, Isosceles), 
-        (4,3,4, Isosceles), 
-        (4,4,3, Isosceles),
-        (3,4,5, Rectangular),
-        (6,12,14, Rectangular),
-        (1, 1000, 1, Other),
-        (15, 100, 2, Other),
-        (0,0,0, NoTriangle),
-        (0, 1, 2, NoTriangle),
-        ((-1), (-2), (-3), NoTriangle),
-        ((-1), 1, 2, NoTriangle)]       
+             (3,4,4, Isosceles), 
+             (4,3,4, Isosceles), 
+             (4,4,3, Isosceles),
+             (3,4,5, Rectangular),
+             (1,1,2, NoTriangle),
+             ((-1), 1, 2, NoTriangle),
+             ((-1), (-2), (-3), NoTriangle),
+             (0,1,2, NoTriangle),
+             (0,0,0, NoTriangle)]
+
+{-- Exercise 3: Testing property strength --}
+
+{-a: Implement all properties from the Exercise 3 from Workshop 2 as Haskell functions of type Int -> Bool. Consider a small domain like [(−10)..10][(−10)..10].} -}
+
+stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+stronger xs p q = forall xs (\ x -> p x --> q x)
+weaker   xs p q = stronger xs q p
+
+property1, property2, property3, property4 :: Int -> Bool
+property1 x = even x && x > 3
+property2 x = even x || x > 3
+property3 x = (even x && x > 3) || even x
+property4 x = even
 
