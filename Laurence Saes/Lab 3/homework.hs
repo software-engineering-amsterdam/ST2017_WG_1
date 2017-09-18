98ae297c4ca8ba4557b16bd58905415d1a0ea471
eigenaar <lauwdude@hotmail.com>
2017-09-18 15:55:04 +0200

    Added documentation

--- Laurence Saes/Lab 3/homework.hs
+++ Laurence Saes/Lab 3/homework.hs
@@ -323,20 +323,35 @@ dsjToCnj :: Form -> Form -> Form
 dsjToCnj (Cnj a) (Cnj b) = Cnj [ Dsj ([subA,subB]) | subA <- da, subB <- db ]
                             where da = map convertToCnf a
                                   db = map convertToCnf b
-dsjToCnj subA (Cnj b) = Cnj [ Dsj ([subA,subB]) | subB <- db ]
+dsjToCnj subA (Cnj b) = Cnj [ Dsj ([da,subB]) | subB <- db ]
                             where db = map convertToCnf b
+                                  da = convertToCnf subA
 dsjToCnj (Cnj a) subB = dsjToCnj subB (Cnj a)
-dsjToCnj subA subB = Dsj [subA, subB]
+dsjToCnj subA subB = Dsj [db, da]
+                            where db = convertToCnf subB
+                                  da = convertToCnf subA
+
+-- Converts disjunction to conjunction:
+-- A disjunction of 2 conjunctions are distributed
+-- A non disjunction is distrubuted over the conjunction
+-- Two non disjunctions will keep the same
 
 cnjToCnj :: Form -> Form -> Form
-cnjToCnj (Cnj a) (Cnj b) = Cnj (concat [a,b])
+cnjToCnj (Cnj a) (Cnj b) = Cnj (concat [normalCnjA,normalCnjB])
                             where normalCnjA = map convertToCnf a
                                   normalCnjB = map convertToCnf b
-cnjToCnj a (Cnj b) = Cnj (a : b)
+cnjToCnj a (Cnj b) = Cnj (normalCnjA : normalCnjB)
+                            where normalCnjA = convertToCnf a
+                                  normalCnjB = map convertToCnf b
 cnjToCnj (Cnj a) b = cnjToCnj b (Cnj a)
-cnjToCnj a b = Cnj [a,b]
+cnjToCnj a b = Cnj [normalCnjA,normalCnjB]
+                            where normalCnjA = convertToCnf a
+                                  normalCnjB = convertToCnf b
 
--- convertToCnf (Cnj a) = (Cnj (foldl (\b x -> (convertToCnf x) : b) [] a))
+-- Two conjunctions will be concatenated
+-- One non conjections with a conjections will be appended
+-- Two non conjunctions will heep the same
+
 convertToCnf :: Form -> Form
 convertToCnf (Prop a) = Prop a
 convertToCnf (Neg (Prop a)) = (Neg (Prop a))
@@ -351,5 +366,13 @@ convertToCnf (Dsj [a]) = convertToCnf a
 convertToCnf (Dsj (a:b:cs)) | length cs == 0 = dsjAB
                             | otherwise = convertToCnf (Dsj (dsjAB:cs))
                                   where dsjAB = dsjToCnj a b
-convertToCnf (Impl a b) = convertToCnf (Dsj [Neg a, b])
-convertToCnf (Equiv a b) = convertToCnf (Dsj [Cnj [a,b], Cnj [Neg a, Neg b]])
+convertToCnf (Impl a b) = convertToCnf (Dsj [Neg (convertToCnf a), (convertToCnf b)])
+convertToCnf (Equiv a b) = convertToCnf (Dsj [Cnj [da,db], Cnj [Neg da, Neg db]])
+                              where da = convertToCnf a
+                                    db = convertToCnf b
+
+-- A property and a negotion with a property is allowed
+-- Negations with a negation will cancle eachother out
+-- Negation on a conjunctions will distribute over all the sub expressions
+-- Negation on a disjunction will distribute over all the sub expressions
+-- An implication and equalivinze will be converted to a disjunction
