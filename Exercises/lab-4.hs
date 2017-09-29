2b5c72b71cd1dfec6c25b770d336d15c8f130f22
Laurence Saes <l.saes@live.nl>
2017-09-29 13:29:07 +0200

    Almost done

--- Exercises/lab-4.hs
+++ Exercises/lab-4.hs
@@ -83,6 +83,51 @@ infixl 9 !!!
 (!!!) :: Eq a => Set a -> Int -> a
 (Set xs) !!! n = xs !! n
 
+-------------- Assignment 2 ---------------
+-- time spent 3 hour
+-- Creates a random Set with a min and max value for each value inside the set
+-- A maximum number of elements inside the set
+-- When it is inposible to create a set (0 till 10 and 11 items) then an empty set is returned
+createTestSet :: Int -> Int -> Int -> IO (Set Int)
+createTestSet mn mx c | mx - mn < c = do return emptySet
+                      | otherwise = do randomSet <- getRandomItems mn mx c
+                                       return (list2set randomSet)
+
+getRandomItems :: Int -> Int -> Int -> IO ([Int])
+getRandomItems _ _ 0 = do return []
+getRandomItems mn mx c = do rn <- randomRIO (mn,mx)
+                            nxt <- getRandomItems mn mx (c-1)
+                            if elem rn nxt
+                              then (getRandomItems mn mx (c))
+                              else return (rn : nxt)
+
+-- createTestSet 0 100 50
+
+-- http://www.cse.chalmers.se/~rjmh/QuickCheck/manual_body.html
+setGenerator :: Int -> Gen (Set Int)
+setGenerator 0 = do return emptySet
+setGenerator n | n>0 = do as <- arbitrary
+                          return (list2set as)
+
+-- To test the sets
+isUnique :: [Int] -> Bool
+isUnique [] = True
+isUnique [_] = True
+isUnique (x:y:zs) = x /= y && isUnique zs
+
+isSet :: (Set Int) -> Bool
+isSet (Set a) = isUnique a
+
+testSet :: IO (Set Int) -> IO Bool
+testSet ist = do st <- ist
+                 return (isSet st)
+
+-- Normal test:
+-- testSet (createTestSet 0 1000 50)
+
+-- QuickCheck
+-- quickCheckWith stdArgs {maxSize=1000} $ forAll (sized setGenerator) isSet
+
 -------------- Assignment 3 ---------------
 -- time spent 2 hour
 
@@ -142,6 +187,8 @@ trCloHelper x [y] = [(x1, y2) | (x1, x2) <- x, (y1, y2) <- trClo (y), x2 == y1]
 
 
 -------------- Assignment 7 ---------------
+-- time spent 1 hour
+
 relationOne :: Rel Int
 relationOne = [(1,2),(2,3),(3,4)]
 
@@ -150,3 +197,61 @@ relationOneTrColResult = [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
 
 symCloTester = symClos relationOne == relationOneSymCloResult
 trCloTester = trClo relationOne == relationOneTrColResult
+
+
+-------------- Assignment 9 ---------------
+-- time spent 4 hours
+type Var = String
+type Env = Var -> Integer
+
+data Expr = I Integer | V Var
+          | Add Expr Expr
+          | Subtr Expr Expr
+          | Mult Expr Expr
+          deriving (Eq)
+
+instance Show Expr where
+  show (I i) = show i
+  show (V v) = show v
+  show (Add r l) = show l ++ " + " ++ show r
+  show (Subtr l r) = show l ++ " - " ++ show r
+  show (Mult l r) = show l ++ " * " ++ show r
+
+data Condition = Prp Var
+               | Eq Expr Expr
+               | Lt Expr Expr
+               | Gt Expr Expr
+               | Ng Condition
+               | Cj [Condition]
+               | Dj [Condition]
+               deriving (Eq)
+
+instance Show Condition where
+  show (Prp i) = show i
+  show (Eq r l) = show l ++ " == " ++ show r
+  show (Lt l r) = show l ++ " > " ++ show r
+  show (Gt l r) = show l ++ " < " ++ show r
+  show (Ng i) = "!" ++ show i
+  show (Cj i) = intercalate " && " (map show i)
+  show (Dj i) = intercalate " || " (map show i)
+
+data Statement = Ass Var Expr
+               | Cond Condition Statement Statement
+               | Seq [Statement]
+               | While Condition Statement
+               deriving (Eq)
+
+instance Show Statement where
+  show (Ass v e) = show v ++ " = " ++ show e
+  show (Cond c sl sr) = "if " ++ (show c) ++ " then " ++ (show sl) ++ " else " ++ (show sr)
+  show (Seq st) = intercalate ";\n" (map show st)
+  show (While c s) = "while (" ++ show c ++ ") then {\n" ++ show s ++ "\n}"
+
+fib :: Statement
+fib = Seq [Ass "x" (I 0), Ass "y" (I 1),
+           While (Gt (V "n") (I 0))
+             (Seq [Ass "z" (V "x"),
+                   Ass "x" (V "y"),
+                   Ass "y" (Add (V "z") (V "y")),
+                   Ass "n" (Subtr (V "n") (I 1))])]
+
